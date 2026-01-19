c489a28997503f6123a2df1a19eb99f2e89716b4
Deig0l <deigolsalazar@gmail.com>
2026-01-19 08:56:45 -0700

    added user auth in MPS section

--- src/app/dashboard/mps/page.tsx
+++ src/app/dashboard/mps/page.tsx
@@ -1,7 +1,12 @@
-export default async function Page() {
+import { auth } from "~/auth";
+import { redirect } from "next/navigation";
 
+export default async function Page() {
+  const session = await auth();
+  if (!session) return redirect("/");
+  
   return ( 
-    <div className="m-14 mt-16 position: 'center'">
+    <div className="m-14 mt-16">
         <h1>MPs Dashboard Coming Soon!</h1>     
     </div>
   );
